3e65ea96cd7a6bf248d437ec03417f07f4b80fc6
Adam Arkuszyński <adam.arkuszynski@gmail.com>
2022-08-22 15:23:13 +0200

    Add gunicorn and configure it

--- models.py
+++ models.py
@@ -10,7 +10,7 @@ class User(Base, UserMixin):
 
     id = Column(Integer, primary_key=True)
     username = Column(String(20), unique=True)
-    email = Column(String(100), unique=True)
+    # email = Column(String(100), unique=True)
     password = Column(String(100))
     registration_date = Column(DateTime, server_default=func.now())
     tables = relationship('Table')
