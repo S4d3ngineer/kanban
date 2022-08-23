bd738a94e377feead5fd65d34aa95a0485706b73
Adam Arkuszyński <adam.arkuszynski@gmail.com>
2022-08-23 10:30:51 +0200

    Include alembic in repo

--- forms.py
+++ forms.py
@@ -1,15 +1,21 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Length, EqualTo
+from wtforms.validators import DataRequired, Length, EqualTo, Regexp
 
 
 class RegisterForm(FlaskForm):
-    username = StringField('username', validators=[DataRequired(), Length(min=3, max=20)])
-    password = PasswordField('password', validators=[DataRequired()])
-    confirm_password = PasswordField('confirm password', validators=[DataRequired(), EqualTo('password')])
+    username = StringField('username', validators=[DataRequired(), Regexp(
+        "^[A-Za-z][A-Za-z0-9_]{3,19}$", message="Username should be 4-20 characters long, "
+        "start with alpha character and can contain only alpha, numeric characters and '_' symbols.")])
+    password = PasswordField('password', validators=[
+                             DataRequired(), Length(min=8, max=40)])
+    confirm_password = PasswordField('confirm password', validators=[
+                                     DataRequired(), EqualTo('password')])
     submit = SubmitField('Register')
 
+
 class LoginForm(FlaskForm):
-    username = StringField('username', validators=[DataRequired(), Length(min=3, max=20)])
+    username = StringField('username', validators=[
+                           DataRequired(), Length(min=3, max=20)])
     password = PasswordField('password', validators=[DataRequired()])
     submit = SubmitField('Login')
